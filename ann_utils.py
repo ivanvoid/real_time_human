aaa4a2a358940c70f26a5d9da5472c13ef441546
Ivan Kakhaev <59028817+ivanvoid@users.noreply.github.com>
2023-11-22 21:24:36 +0800

    annotations working

---
+++ ann_utils.py
@@ -0,0 +1,22 @@
+import os
+import cv2
+from PIL import Image
+
+def save_point(x,y,id, cfg):
+    name = ('00000000' + str(id))[-6:] + '.png'
+    path = cfg.data_path + 'annotations.csv'
+    
+    if not os.path.exists(path):
+        with open(path,'w') as f:
+            f.write('file_name,image_id,x,y\n')
+    
+    with open(path,'a') as f:
+        f.write("{:},{:},{:},{:}\n".format(name, id,x,y))
+        
+
+def save_image(img, id, cfg):
+    name = ('00000000' + str(id))[-6:] + '.png'
+    path = cfg.data_path + 'images/' + name
+    print(path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    Image.fromarray(img).save(path)
